631f7164fe2d47653fcf5adbec3f1b4bbf1f0a8d
khushiarora11 <khushii.aroraa11@gmail.com>
2025-03-25 02:11:26 +0000

    Update progress.tsx

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 
-import *ProgressPrimitive from "@radix-ui/react-progress"
+import * as ProgressPrimitive from "@radix-ui/react-progress"
 
 import { cn } from "@/lib/utils"
 
@@ -19,11 +19,10 @@ const Progress = React.forwardRef<
   >
     <ProgressPrimitive.Indicator
       className={cn("h-full w-full flex-1 bg-primary transition-all", indicatorClassName)}
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+      style={{ transform: translateX(-${100 - (value || 0)}%) }}
     />
   </ProgressPrimitive.Root>
 ))
 Progress.displayName = ProgressPrimitive.Root.displayName
 
-export { Progress }
-
+export { Progress } 
